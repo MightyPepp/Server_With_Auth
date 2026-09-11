d9ca2a732c9ac71d9428bed4750a426e0b54c619
Aleksandr_Samsonov <samsonovaleksandr1352006@gmail.com>
2026-09-11 19:18:59 +0300

    Написали отдельный скрипт, проверяющий наличие комплекта ключей и сертификатов в /etc/ssl/self_signed

---
+++ BasicAuthTLSHashedPassword/serverTLSHashedPassword/scripts/existanceCrtsAndKeys.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Порядок параметров CLI:
+# ca.key ca.crt server.key server.crt
+
+cd /etc/ssl/self_signed
+
+CAkey="private/${1:-ca.key}"
+CAcrt="certs/${2:-ca.crt}"
+SRVkey="private/${3:-server.key}"
+SRVcrt="certs/${4:-server.crt}"
+
+if ! [ -e "$CAkey" ] || ! [ -e "$CAcrt" ] || ! [ -e "$SRVkey" ] || ! [ -e "$SRVcrt" ]; then
+    rm -f $CAkey $CAcrt $SRVkey $SRVcrt
+    echo "Удалили всё, чтобы заново нагенерить!"
+    /home/mighty-pepe/Desktop/Server_With_Auth/BasicAuthTLSHashedPassword/serverTLSHashedPassword/scripts/genKeys.sh
+else 
+    echo "Все файлы ключей и сертификатов на месте!"
+fi
